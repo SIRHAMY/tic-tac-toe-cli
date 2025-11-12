3ac7b753b4c7b24e1e122d11bbb7084e304688a5
HAMY <hamy@hamy.xyz>
2025-11-12 14:59:14 -0500

    Cleanup: Add test

---
+++ TicTacToe.Tests/TicTacToeTests.cs
@@ -0,0 +1,18 @@
+﻿namespace TicTacToe.Tests;
+
+public class TicTacToeTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var gameEngine = new TicTacToeGame();
+
+        var move = new TicTacToeMove(
+            Player: Player.Player1,
+            X: 0,
+            Y: 0
+        );
+        var result = gameEngine.MakeMove(move);
+        Assert.True(result.IsSuccess);
+    }
+}
